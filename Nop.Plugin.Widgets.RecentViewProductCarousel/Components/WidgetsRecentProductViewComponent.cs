817ce1c88e801c8c07a3e285112f70cb158ce5bd
Hardik Gohil <krishna@adwebsoft.com>
2022-01-03 17:16:59 +0530

    Add recent viewed product

---
+++ Nop.Plugin.Widgets.RecentViewProductCarousel/Components/WidgetsRecentProductViewComponent.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core.Domain.Catalog;
+using Nop.Services.Catalog;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.RecentViewProductCarousel.Components
+{
+    [ViewComponent(Name = "RecentViewProductCarousel")]
+    public class ExampleWidgetViewComponent : NopViewComponent
+    {
+        #region Fields
+
+        private readonly IProductModelFactory _productModelFactory;
+        private readonly IProductService _productService;
+        private readonly IRecentlyViewedProductsService _recentlyViewedProductsService;
+        private readonly CatalogSettings _catalogSettings;
+
+        #endregion
+
+        #region Ctor
+
+        public ExampleWidgetViewComponent(IProductModelFactory productModelFactory, IProductService productService, 
+            IRecentlyViewedProductsService recentlyViewedProductsService, CatalogSettings catalogSettings)
+        {
+            _productModelFactory = productModelFactory;
+            _productService = productService;
+            _recentlyViewedProductsService = recentlyViewedProductsService;
+            _catalogSettings = catalogSettings;
+        }
+
+        #endregion
+
+        public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
+        {
+            var products = await _recentlyViewedProductsService.GetRecentlyViewedProductsAsync(_catalogSettings.RecentlyViewedProductsNumber);
+
+            var model = new List<Web.Models.Catalog.ProductOverviewModel>();
+            model.AddRange(await _productModelFactory.PrepareProductOverviewModelsAsync(products));
+
+            return View("~/Plugins/Widgets.RecentViewProductCarousel/Views/PublicInfo.cshtml", model);
+        }
+    }
+}
---
+++ Nop.Plugin.Widgets.RecentViewProductCarousel/RecentViewProductCarousel.cs
@@ -0,0 +1,61 @@
+﻿using Nop.Core.Domain.Catalog;
+using Nop.Core.Infrastructure;
+using Nop.Services.Cms;
+using Nop.Services.Configuration;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.RecentViewProductCarousel
+{
+    public class RecentViewProductCarousel : BasePlugin, IWidgetPlugin
+    {
+        /// <summary>
+        /// Gets a value indicating whether to hide this plugin on the widget list page in the admin area
+        /// </summary>
+        public bool HideInWidgetList => false;
+
+        /// <summary>
+        /// Gets a name of a view component for displaying widget
+        /// </summary>
+        /// <param name="widgetZone">Name of the widget zone</param>
+        /// <returns>View component name</returns>
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "RecentViewProductCarousel";
+        }
+
+        /// <summary>
+        /// Gets widget zones where this widget should be rendered
+        /// </summary>
+        /// <returns>
+        /// A task that represents the asynchronous operation
+        /// The task result contains the widget zones
+        /// </returns>
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.HomepageBeforeNews });
+        }
+
+        public override async Task InstallAsync()
+        {
+            var settingService = EngineContext.Current.Resolve<ISettingService>();
+            await settingService.SaveSettingAsync(new CatalogSettings
+            {
+                RecentlyViewedProductsNumber = 5
+            });
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            //Logic during uninstallation goes here...
+
+            await base.UninstallAsync();
+        }
+    }
+}
